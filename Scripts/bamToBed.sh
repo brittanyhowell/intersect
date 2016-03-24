6712cc2942394af09dcc9e34f39ace544406b4c1
brittanyhowell <brittanyhowell@DC02R50RQGG77.ad.adelaide.edu.au>
2016-03-24 17:31:57 +1030

    added 'module load' and out.log

--- Scripts/bamToBed.sh
+++ Scripts/bamToBed.sh
@@ -1,6 +1,25 @@
 #!/bin/bash
 
-FILEPATH=/data/rc003/Brittany/intersect/bam
-OUTPATH=/data/rc003/Brittany/intersect/bed
+# Invoked by:
+#
+# FILEPATH=/data/rc003/Brittany/intersect/bam/Henmt1mRNAseq_mouse OUTPATH=/data/rc003/Brittany/intersect/bed/ sbatch bamToBed.sh
+#
 
-for f in (ls ${FILEPATH}); do  (bedtools bamtobed -i $f > ${OUTPATH}/(ls ${FILEPATH}).bed); done
+#SBATCH -p batch
+#SBATCH -N 1 
+#SBATCH -n 8  				# Asking for cores
+#SBATCH --time=1-00:00  	# Days taken
+#SBATCH --mem=30GB 			# memory thought to be required
+
+# Notification configuration 
+#SBATCH --mail-type=END                                         
+#SBATCH --mail-type=FAIL                                        
+#SBATCH --mail-user=brittany.howell@student.adelaide.edu.au      
+
+
+# Load the necessary modules
+module load BEDTools/2.25.0-foss-2015b
+
+
+for f in ls ${FILEPATH}; do bedtools bamtobed -i $f > ${OUTPATH}/$f.bed; done
+2> out.log
