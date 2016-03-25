2ba196abec51e7830a5b5ce9d496d3347b754a73
brittanyhowell <brittanyhowell@DC02R50RQGG77.ad.adelaide.edu.au>
2016-03-25 21:00:46 +1030

    for loop now uses .bams only

--- Scripts/bamToBed.sh
+++ Scripts/bamToBed.sh
@@ -2,7 +2,7 @@
 
 # Invoked by:
 #
-# FILEPATH=/data/rc003/Brittany/intersect/bam/Henmt1mRNAseq_mouse OUTPATH=/data/rc003/Brittany/intersect/bed/ sbatch bamToBed.sh
+# FILEPATH=/data/rc003/Brittany/intersect/bam/Henmt1mRNAseq_mouse OUTPATH=/data/rc003/Brittany/intersect/bed/  sbatch bamToBed.sh
 #
 
 #SBATCH -p batch
@@ -16,10 +16,16 @@
 #SBATCH --mail-type=FAIL                                        
 #SBATCH --mail-user=brittany.howell@student.adelaide.edu.au      
 
+#FILE=$(ls $FILEPATH)
 
 # Load the necessary modules
 module load BEDTools/2.25.0-foss-2015b
 
+for f in *.bam ; do 
 
-for f in ls ${FILEPATH}; do bedtools bamtobed -i $f > ${OUTPATH}/$f.bed; done
+bedtools bamtobed -i $f > $f.bed;
+
+ done
 2> out.log
+
+
